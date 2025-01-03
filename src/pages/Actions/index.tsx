6c3d8150452f8dfc1f6dcb51e13bc81c55f81b66
luanacdev <luana.cam20@gmail.com>
2025-01-03 13:43:11 -0300

    update status color and graphics

--- src/pages/Actions/index.tsx
+++ src/pages/Actions/index.tsx
@@ -12,17 +12,9 @@ export function Actions() {
   const { isLoading } = useActions();
   const { getUserOptions } = useCompanyUsers();
 
-  if (isLoading) {
-    return (
-      <LayoutLogged>
-        <S.LoadingContainer>
-          <S.LoadingText>Carregando...</S.LoadingText>
-        </S.LoadingContainer>
-      </LayoutLogged>
-    );
-  }
-
-  getUserOptions();
+  useEffect(() => {
+    getUserOptions();
+  }, []);
 
   return (
     <LayoutLogged>
@@ -32,46 +24,53 @@ export function Actions() {
           <S.HeaderSection>
             <S.UserSection>
               <S.Avatar>
-                {user?.name.charAt(0)}
+                {isLoading ? "..." : user?.name.charAt(0)}
               </S.Avatar>
               <S.UserInfo>
                 <S.WelcomeText>Bem-vindo(a) de volta,</S.WelcomeText>
                 <S.CompanyName>
-                  {user?.user_type_id === 3 && infoCompany
-                    ? infoCompany.fantasy_name
-                    : user?.customer[0].fantasy_name}
+                  {isLoading ? (
+                    <S.LoadingText>Carregando...</S.LoadingText>
+                  ) : (
+                    user?.user_type_id === 3 && infoCompany
+                      ? infoCompany.fantasy_name
+                      : user?.customer[0].fantasy_name
+                  )}
                 </S.CompanyName>
                 <S.UserRole>
-                  {user?.name}
-                  <S.RoleTag>
-                    {(() => {
-                      switch (user?.user_type_id) {
-                        case 1:
-                          return "Colaborador";
-                        case 2:
-                          return "Gestor";
-                        case 3:
-                          return "Consultor";
-                        case 4:
-                          return "Administrador";
-                        default:
-                          return "Colaborador";
-                      }
-                    })()}
-                  </S.RoleTag>
+                  {isLoading ? (
+                    <S.LoadingText>Carregando...</S.LoadingText>
+                  ) : (
+                    <>
+                      {user?.name}
+                      <S.RoleTag>
+                        {(() => {
+                          switch (user?.user_type_id) {
+                            case 1:
+                              return "Colaborador";
+                            case 2:
+                              return "Gestor";
+                            case 3:
+                              return "Consultor";
+                            case 4:
+                              return "Administrador";
+                            default:
+                              return "Colaborador";
+                          }
+                        })()}
+                      </S.RoleTag>
+                    </>
+                  )}
                 </S.UserRole>
               </S.UserInfo>
             </S.UserSection>
           </S.HeaderSection>
-
-
 
           {/* Board de Ações */}
           <S.BoardSection>
             <TableActions />
           </S.BoardSection>
         </S.Content>
-
       </S.Container>
     </LayoutLogged>
   );
